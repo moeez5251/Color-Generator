d22076f155047541bb7fbfc70f712291d58f054d
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-07-21 16:22:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let head = document.querySelector('.head');
+let button = document.querySelector('.button');
+let heading = document.querySelector('.heading');
+const value = "Random Color Generator"
+let pallete = document.querySelectorAll('.pallete');
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        button.click();
+        head.style.top = '-100%';
+    }, 2400);
+})
+
+
+for (let i = 0; i < value.length; i++) {
+    setTimeout(() => {
+        heading.innerHTML += value[i];
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);// 16777215 is max 24 bit value 
+        heading.style.color = "#" + randomColor;
+    }, i * 95);
+}
+
+const palltete_reset = () => {
+    pallete.forEach(e => {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        let color_element = e.querySelector('.color');
+        color_element.style.backgroundColor = '#' + randomColor;
+        let span_element = e.querySelector('.name');
+        span_element.innerHTML = '#' + randomColor;
+
+    });
+}
+button.addEventListener('click', palltete_reset);
